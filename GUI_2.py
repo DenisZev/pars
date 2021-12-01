dd335fde4f35c0b5e8256cfef3cbcf76af24b8c9
zevae <271k@bk.ru>
2021-12-01 21:35:51 +0700

    ...

--- GUI_2.py
+++ GUI_2.py
@@ -1,9 +1,11 @@
+import tkinter
 from tkinter import *
 from tkinter import ttk
 import sqlite3
 import requests
 import time
 from bs4 import BeautifulSoup
+import webbrowser
 
 # соединение с базой данных
 connection = sqlite3.connect(':memory:')
@@ -25,15 +27,15 @@ class drom(object):
         brand = combo_1.get()  # input('Введите марку автомобиля на английском ')
         model = l1_entry.get()  # input('Введите модель ')
         lowprice = int(combo_2.get())  # int(input('Введите примерную стоимость '))
-        minprice = lowprice - 50000
-        maxprice = lowprice + 50000
+        minprice = lowprice - 20000
+        maxprice = lowprice + 20000
         a = 1
 
         if lowprice < 50000:
             maxprice = 100000
         if model == '':
             model = 'all'
-        for j in range(1, 3):
+        for j in range(1, 5):
             # a+=a
             # указываем url и get параметры запроса
             url = f'https://auto.drom.ru/{brand}/{model}/page{j}/?minprice={minprice}&maxprice={maxprice}&?ph=1&pts=2&damaged=2&unsold=1&order=price'
@@ -66,10 +68,8 @@ class drom(object):
 # languages_listbox.delete(selection[0])
 
 
-# добавление нового элемента
+# добавление в таблицу
 def add():
-    # new_language = l1_entry.get()
-    # languages_listbox.insert(0, new_language)
     cursor.execute("SELECT * FROM car_search")
     i = 0
     rows = cursor.fetchall()
@@ -78,15 +78,23 @@ def add():
         tree.insert("", 'end', values=(row[0], row[1], row[2], row[3]))
 
 
-# box_value = StringVar()
+# Поиск
+def search():
 
-
-def run():
     sqlite_update_table_query = 'DELETE from car_search'
     cursor.execute(sqlite_update_table_query)
     for i in tree.get_children():
         tree.delete(i)
     drom()
+
+
+def print_selection(event):
+    for selection in tree.selection():
+        item = tree.item(selection)
+        links = item["values"][3]
+        text = "Выбор: {}, {} <{}>"
+        # print(links)
+        webbrowser.open_new(links)
 
 
 root = Tk()
@@ -99,7 +107,7 @@ combo_1 = ttk.Combobox(root,
                            "volvo",
                            "mercedes-benz"])
 
-combo_1.grid(column=0, row=1)
+combo_1.grid(column=0, row=1, padx=6, pady=6)
 combo_1.current(1)
 combo_2 = ttk.Combobox(root,
                        values=[
@@ -109,37 +117,38 @@ combo_2 = ttk.Combobox(root,
                            1000000,
                            1500000])
 
-combo_2.grid(column=1, row=1)
+combo_2.grid(column=0, row=5, padx=3, pady=3)
 combo_2.current(1)
 # текстовое поле и кнопка для добавления в список
 lb_1 = Label(text='Введите марку автомобиля')
 lb_1.grid(column=0, row=0)
 l1_entry = Entry(width=23)
-l1_entry.grid(column=0, row=3, padx=6, pady=6)
+l1_entry.grid(column=0, row=3, padx=2, pady=2)
 lb_2 = Label(text='Введите модель автомобиля')
 lb_2.grid(column=0, row=2)
 lb_3 = Label(text='Выберите стоимость')
-lb_3.grid(column=1, row=0)
-add_button = Button(text="Заполнить таблицу", command=add).grid(column=2, row=3, padx=6, pady=6)
+lb_3.grid(column=0, row=4)
+add_button = Button(text="Заполнить таблицу", command=add, bg='lightblue').grid(column=0, row=7, padx=2, pady=2)
+search_button = Button(text="Запрос", command=search, bg='lightblue').grid(row=6, column=0, padx=2, pady=2)
+# создаем таблицу
 
-# создаем список
-# languages_listbox = Listbox()
-# languages_listbox.grid(row=4, column=0, columnspan=3, sticky=W + E, padx=10, pady=10)
 tree = ttk.Treeview(columns=('carName', 'price', 'description', 'links'), show='headings')
 tree.column('carName', width=120)
 tree.column('price', width=70)
 tree.column('description', width=350)
-tree.column('links')
+tree.column('links', width=350)
+tree.bind("<<TreeviewSelect>>", print_selection)
 tree.heading('carName', text='Название')
 tree.heading('price', text='Стоимость')
 tree.heading('description', text='Описание')
 tree.heading('links', text='Ссылка')
+# ysb = ttk.Scrollbar("", orient=VERTICAL, command=tree.yview())
+# tree.configure(yscroll=ysb.set)
+tree.grid(column=1, row=0, rowspan=10, padx=6, pady=6)
 
-tree.grid(column=0, row=6, padx=6, pady=6)
-# добавляем в список начальные элементы
-# languages_listbox.insert(END, "Python")
-# languages_listbox.insert(END, "C#")
 
-delete_button = Button(text="Запрос", command=run).grid(row=2, column=1, padx=5, pady=5)
+def callback(event):
+    webbrowser.open_new(event.widget.cget("text"))
+
 
 root.mainloop()
